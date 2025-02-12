aabef1d43825b6ee3a251c6643a8eb93a62194f9
stvishw <stvishw@gmail.com>
2025-02-12 07:37:00 +0530

    changes in db

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-# Replace with your actual MongoDB Atlas connection string
+# Replace with your password MongoDB Atlas connection string
 MONGO_URI = "mongodb+srv://user>:<password>@audio-project.faluj.mongodb.net/?retryWrites=true&w=majority&appName=audio-project"
 
 try:
@@ -9,7 +9,7 @@ try:
     db = client["fingerprint_db"]  # Database name
     fingerprint_collection = db["fingerprint_data"]  # Collection name
 
-    # Insert a test document to ensure the collection is created
+    # Collection is created
     if fingerprint_collection.count_documents({}) == 0:
         fingerprint_collection.insert_one({"message": "Collection created successfully!"})
 
